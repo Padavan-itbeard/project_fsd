bbe309c71254ba7cb63f680f7f8ec127de13fbff
Alexandr Ivanov <Alexandr.ivanov.1024@gmail.com>
2022-09-14 23:42:15 +0300

    webpack config restructuring

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,25 @@
+import path from "path";
+import { Configuration } from "webpack";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+import { BuildOptions } from "./types/config";
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+  const { mode, paths } = options;
+
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+  };
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,21 @@
 import path from "path";
 import webpack from "webpack";
-import HTMLWebpackPlugin from "html-webpack-plugin";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildPaths } from "./config/build/types/config";
 
-const webpackConfig: webpack.Configuration = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src/index.ts"),
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new webpack.ProgressPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
+const paths: BuildPaths = {
+  entry: path.resolve(__dirname, "src", "index.ts"),
+  html: path.resolve(__dirname, "public", "index.html"),
+  build: path.resolve(__dirname, "build"),
 };
+
+const mode = "development";
+const isDev = mode === "development";
+
+const webpackConfig: webpack.Configuration = buildWebpackConfig({
+  mode: "development",
+  paths,
+  isDev
+});
 
 export default webpackConfig;
