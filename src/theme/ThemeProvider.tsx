f38eac3024e52c07153417ae8006ee6fc86e6ee3
Alexandr Ivanov <Alexandr.ivanov.1024@gmail.com>
2022-10-02 09:19:03 +0300

    add Theme Provider

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,29 @@
+import { FC, useMemo, useState } from "react";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+
+const defaultValue =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) ?? Theme.LIGHT;
+
+const ThemeProvider: FC = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(defaultValue);
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+  };
+
+  const defaultProps = useMemo(
+    () => ({
+      theme: theme,
+      setTheme: setTheme,
+    }),
+    [theme]
+  );
+
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
